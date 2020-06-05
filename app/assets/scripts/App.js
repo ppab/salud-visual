fb1ffef78dd1b6652b5e9e44ab71570374d2db8c
ppab <josearmentab@gmail.com>
2020-06-05 16:44:37 -0500

    smooth scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,23 @@
 
 import '../styles/styles.css';
+
+
+//Get al a Href
+var ahrefs=document.querySelectorAll(`a[href]`);
+//Create a Click Listener for each a Href
+ahrefs.forEach(element=>{
+   element.addEventListener("click",(e)=>{
+       //Just use aHref elements that have dataset.goto
+    if (e.target.dataset.goto){
+        //prevent defalut operation
+        e.preventDefault();
+       var id=e.target.dataset.goto;
+       var scrollElement=document.getElementById(id);
+        //go to element
+       scrollElement.scrollIntoView({behavior: "smooth"});
+      
+    }
+   
+   })
+});
+
